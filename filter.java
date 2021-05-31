b3c583c4d2f4e593e30177ddb60b5570b80b4fcc
Riya12sharma <79782203+Riya12sharma@users.noreply.github.com>
2021-05-31 11:12:34 +0530

    Create filter.java

---
+++ filter.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author riya
+ */
+public class filter1 implements Filter {
+
+public void init(FilterConfig arg0) throws ServletException {
+
+}
+
+public void doFilter(ServletRequest req, ServletResponse resp,FilterChain chain)                              			    throws IOException, ServletException 
+{ PrintWriter out=resp.getWriter();
+  out.print("filter is invoked before");//exe. with request
+  chain.doFilter(req, resp);//send request to nextresource
+  out.print("filter is invoked after");//exe. with response
+}
+public void destroy() {
+
+}
+}
+
+
+    
